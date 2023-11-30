54913eb4914d02ad7397ab8124d18699fe6ce14b
BRIWA <salaheddinezouitni00@gmail.com>
2023-11-30 20:30:17 +0100

    arabic lang added

---
+++ lang/ar/experience.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+"added_success"=>"تمت الإضافة بنجاح",
+"title_required"=>"العنوان مطلوب",
+"company_name_required" =>"اسم الشركة مطلوب",
+"employement_type_required" =>"نوع الوظيفة مطلوب",
+"location_required" =>"الموقع مطلوب",
+"location_type_required" =>"نوع الموقع مطلوب",
+"start_date_required" =>"تاريخ البدء مطلوب",
+"end_date_required" =>"تاريخ الانتهاء مطلوب",
+"description_required" =>"الوصف مطلوب",
+"user_id_invalid" =>"معرف المستخدم غير صالح",
+"experiences_fetched" =>"تم جلب الخبرات"
+];
